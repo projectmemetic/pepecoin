963244c381fc7f64261abc939a3b6c0a60e9f35c
PepeCoin 2016 <cyberpepe@users.noreply.github.com>
2016-03-28 00:08:07 -0600

    Update version names

--- src/version.cpp
+++ src/version.cpp
@@ -8,10 +8,10 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("-Tranf");
+const std::string CLIENT_NAME("-Txfix");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-BabyPepe"
+#define CLIENT_VERSION_SUFFIX   "-Txfixprebrand"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
