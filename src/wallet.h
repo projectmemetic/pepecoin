a60268dcf2ca189bd962288106a96b8c240e4137
Memetic Project <memetic@memeticproject.com>
2016-04-03 03:48:50 -0600

    Add WalletUpdateSpent header function for staking display fixes

--- src/wallet.h
+++ src/wallet.h
@@ -257,6 +257,7 @@ public:
     bool AddToWallet(const CWalletTx& wtxIn, bool fFromLoadWallet=false);
     void SyncTransaction(const CTransaction& tx, const CBlock* pblock, bool fConnect = true);
     bool AddToWalletIfInvolvingMe(const CTransaction& tx, const CBlock* pblock, bool fUpdate);
+    void WalletUpdateSpent(const CTransaction& prevout, bool fBlock = false);
     void EraseFromWallet(const uint256 &hash);
     int ScanForWalletTransactions(CBlockIndex* pindexStart, bool fUpdate = false);
     void ReacceptWalletTransactions();
