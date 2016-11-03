4a6454d55e9562b26cc722318ded5a6824e8226a
Memetic Project / PepeCoin <memetic@memeticproject.com>
2016-11-02 22:00:10 -0600

    Update revision number

--- src/version.cpp
+++ src/version.cpp
@@ -8,10 +8,10 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Memetic-Core-r6-61405");
+const std::string CLIENT_NAME("Memetic-Core-rev7-61405");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-POSOnly"
+#define CLIENT_VERSION_SUFFIX   "-SteakSauce"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
