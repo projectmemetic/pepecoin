5da14e6b1b399597d959887650fc484b2a00d840
memeticproject <memetic@memeticproject.com>
2016-12-11 22:25:53 -0500

    Increase message wall display a bit

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -407,8 +407,8 @@ void OverviewPage::getMessages()
 {
     LogPrintf("getMessages\n");
     ui->listWidget->clear();
-    //get messages from last 120 blocks
-    int i = 12000;
+    //get messages from last 200 blocks, cache this to disk in future because going back too far causes slowdowns
+    int i = 20000;
     CBlockIndex* pindex = pindexBest;
     while(i > 0 && pindex != NULL)
     {
