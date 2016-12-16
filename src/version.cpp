0d0d82046f27fef04ed6932b91eee778f54f38c4
Memetic Project / PepeCoin <memetic@memeticproject.com>
2016-12-15 19:49:14 -0700

    Update version suffix

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Memetic-Core-1.8-61406");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-SteakSauce"
+#define CLIENT_VERSION_SUFFIX   "-Shadilay"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
