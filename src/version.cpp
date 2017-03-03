82d1a7f21faa35cae46b6931915a50e0a362e5cb
pepeteam <pepecoin@tutanota.com>
2017-03-03 01:34:52 -0700

    Version number 2.0.0.0 increase

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("PepeCoin-Core-1.8.8-61407");
+const std::string CLIENT_NAME("PepeCoin-2.0.0.0");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-Shadilay"
