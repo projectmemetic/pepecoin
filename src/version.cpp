af1da7567ccccb81869b53e302e70ceb47782628
pepeteam <pepecoin@tutanota.com>
2017-03-28 20:53:12 -0600

    correct version number, keep for compatibility for now

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("PepeCoin-2.0.0.0");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "61408"
+#    define GIT_COMMIT_ID "61407"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
