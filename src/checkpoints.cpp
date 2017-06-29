09fe12a6c47b0b62ecf987845325f5dff10c55bc
projectmemetic <projectmemetic@hmamail.com>
2017-06-28 23:15:20 -0400

    Fix stake masternode payment detection, add hard checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -52,6 +52,7 @@ namespace Checkpoints
         (581777, uint256("0xf578368df2ca18fe517931d2a51c9cc2a6c5e00b168be4773786ce80e33ed47e"))     
         (600000, uint256("0x69374e4dd79947ac885587b80a2730efef922772b01494885e95c533f415c46a"))
         (736820, uint256("0x40beb7ceb66dd20e22854a77e020bc76bf401c92e3d1e455d711067ab0de0d81"))
+        (738522, uint256("0x8cfc6234f870351ab359a5f8d116af4e0203a50de8a111016f081902da2a782e"))
     ;
 
     // TestNet has no checkpoints
