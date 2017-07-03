e3437d9a6ba20ef11df015fe125497d42d291c9a
projectmemetic <projectmemetic@hmamail.com>
2017-07-02 20:55:56 -0400

    Networking enhancements

--- src/net.cpp
+++ src/net.cpp
@@ -1102,11 +1102,11 @@ void ThreadDNSAddressSeed()
 {
     // goal: only query DNS seeds if address need is acute
     if ((addrman.size() > 0) &&
-        (!GetBoolArg("-forcednsseed", true))) {
+        (!GetBoolArg("-forcednsseed", false))) {
         MilliSleep(11 * 1000);
 
         LOCK(cs_vNodes);
-        if (vNodes.size() >= 2) {
+        if (vNodes.size() >= 4) {
             LogPrintf("P2P peers available. Skipped DNS seeding.\n");
             return;
         }
