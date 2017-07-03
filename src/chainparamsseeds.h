604a30aac1eba2bb46290bc7c2048c34ca18c8e7
projectmemetic <projectmemetic@hmamail.com>
2017-07-02 21:03:55 -0400

    seeds

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -5,7 +5,7 @@
 
 // Each line contains a 16-byte IPv6 address and a port.
 // IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.
-static const unsigned int pnSeed[] =
+static const SeedSpec6 pnSeed6_main[] =
 {
   // We will do this in DNS instead - seed.pepecoin.co
   {{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xff,0x6b,0xaa,0x37,0xd3}, 29377},
