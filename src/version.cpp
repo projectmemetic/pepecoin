10133baeeb7b0e058e9c74296896dece33bef6bc
cyberpepe <kekronet@gmail.com>
2017-11-08 03:52:40 -0700

    Version number 2.6.0.0

--- src/version.cpp
+++ src/version.cpp
@@ -8,10 +8,10 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("PepeCoin-2.5.2.0");
+const std::string CLIENT_NAME("PepeCoin-Memetic-2.6.0.0");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-KekdaqMT"
+#define CLIENT_VERSION_SUFFIX   "-Launchpad"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
