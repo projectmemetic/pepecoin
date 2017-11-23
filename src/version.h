7be95b9d4e8a3ceae546d3ef65f0b1fd5b608a41
cyberpepe <kekronet@gmail.com>
2017-11-23 02:48:50 -0700

    set version 2.6.2.0

--- src/version.h
+++ src/version.h
@@ -36,19 +36,19 @@ static const int PROTOCOL_VERSION = 61414;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 61413;
+static const int MIN_PEER_PROTO_VERSION = 61414;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 61402;
+static const int MIN_POOL_PEER_PROTO_VERSION = 61414;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 61402;
-static const int MIN_MN_PROTO_VERSION = 61402;
+static const int MIN_INSTANTX_PROTO_VERSION = 61414;
+static const int MIN_MN_PROTO_VERSION = 61414;
 
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 61402;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 61402;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 61414;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 61414;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
