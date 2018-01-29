db273ccb704ceebcdccb6be850ab78c3bb6084b6
cyberpepe <kekronet@gmail.com>
2018-01-28 22:23:14 -0700

    Version 2.7.0.0 preparation

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("PepeCoin-Memetic-2.6.2.0");
+const std::string CLIENT_NAME("PepeCoin-Memetic-2.7.0.0");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-Launchpad"
