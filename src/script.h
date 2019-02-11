f80519a01fd6b5d91fd34f060052bf09b5ad2b04
cyberpepe <kekronet@gmail.com>
2019-02-11 02:55:28 -0500

    Increase OP_RELAY data size from 40 bytes to 160 bytes

--- src/script.h
+++ src/script.h
@@ -27,7 +27,7 @@ class CTransaction;
 class BaseSignatureChecker;
 
 static const unsigned int MAX_SCRIPT_ELEMENT_SIZE = 520; // bytes
-static const unsigned int MAX_OP_RETURN_RELAY = 40;      // bytes
+static const unsigned int MAX_OP_RETURN_RELAY = 160;      // bytes
 
 template <typename T>
 std::vector<unsigned char> ToByteVector(const T& in)
