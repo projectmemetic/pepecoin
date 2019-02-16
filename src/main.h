502cad784a3ce5c9a7e518b61ee14d5bf9a822c9
projectmemetic <projectmemetic@hmamail.com>
2019-02-15 23:07:24 -0500

    Fix capitalization of nTime

--- src/main.h
+++ src/main.h
@@ -148,7 +148,7 @@ inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MO
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
 static const int64_t DRIFT = 600;
-inline int64_t FutureDrift(int64_t nTime, int nHeight) { return nHeight >= PEPE_DIFFIMPROVE_HEIGHT ? ntime + 60 : nTime + DRIFT; }
+inline int64_t FutureDrift(int64_t nTime, int nHeight) { return nHeight >= PEPE_DIFFIMPROVE_HEIGHT ? nTime + 60 : nTime + DRIFT; }
 
 /** "reject" message codes **/
 static const unsigned char REJECT_INVALID = 0x10;
