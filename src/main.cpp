abdd2b2daec3e2e95a4764d79d82183b03947df4
projectmemetic <projectmemetic@hmamail.com>
2019-02-25 13:20:23 -0500

    Trigger cached balance update on new accepted block, in case coins matured out of stake

--- src/main.cpp
+++ src/main.cpp
@@ -3493,6 +3493,10 @@ bool ProcessBlock(CNode* pfrom, CBlock* pblock)
     }
     LogPrintf("ProcessBlock: ACCEPTED Height: %d Difficulty: %d\n", mapBlockIndex[hash]->nHeight, dDiff);
 
+#ifdef ENABLE_WALLET
+    pwalletMain->fCachedBalanceNeedsUpdating = true;  // update cached balance in case coins came out of stake with the accepted block
+#endif
+
     return true;
 }
 
